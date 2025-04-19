f7edbd993c4f0209173b117ca30eccb910014f33
OmarHashsh <mohamadtabbakh.swi@gmail.com>
2025-04-19 17:06:39 +0300

    New DTO for Reminders. Necessary for EventServices.cs

---
+++ Core/Calelytic.Core/DTOs/ReminderDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calelytic.Core.Enums;
+using Calelytic.Core.Models;
+
+namespace Calelytic.Core.DTOs
+{
+    public class ReminderDTO
+    {
+        public int ReminderId { get; }
+        public TimeSpan TimeBeforeEvent { get; set; }
+        public ReminderMethod Method { get; set; }
+        public int EventId { get; set; }
+    }
+}
