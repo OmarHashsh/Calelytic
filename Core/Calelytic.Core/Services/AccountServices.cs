f7fa8958f487a5f0077e5d42c773500b74eda4de
OmarHashsh <mohamadtabbakh.swi@gmail.com>
2025-04-19 11:04:06 +0300

    Improved testability by seperating the testing layer from the services layer.

--- Core/Calelytic.Core/Services/AccountServices.cs
+++ Core/Calelytic.Core/Services/AccountServices.cs
@@ -53,26 +53,21 @@ namespace Calelytic.Core.Services
 
         // Fetch data of some account and return to caller.
         // *1 *2
-        public async Task<Guid> FetchAccDataByEmail(string email)
+        public async Task<Guid?> FetchAccDataByEmail(string email)
         {
             var accountId = await _context.Accounts
                 .Where(a => a.Email == email)
                 .Select(a => a.Id)
                 .FirstOrDefaultAsync();
-
-            if (accountId == Guid.Empty)
-                throw new NotFoundException("Account not found.");
-            return accountId;
+            return accountId == Guid.Empty ? null : accountId;
         }
         // Modify account
         // *1
-        public async Task ModifyAccount(Guid id, AccountDTO newData)
+        public async Task<bool> ModifyAccount(Guid id, AccountDTO newData)
         {
             var dataInDB = await _context.Accounts.FindAsync(id);
             if (dataInDB == null)
-            {
-                throw new NotFoundException("Account not found.");
-            }
+                return false;
 
             var nonEditableProps = new HashSet<string>
             {
@@ -97,18 +92,21 @@ namespace Calelytic.Core.Services
                 }
             }
             await _context.SaveChangesAsync();
+            return true;
         }
 
         // Soft Delete an account ( Gives a chance for someone to undo this action but permanently deletes the account after 30 days)
         // *1
-        public async Task SoftDeleteByGuid(Guid Id)     // replace the value passed to Task with the appropriate value for my purpose
+        public async Task<bool> SoftDeleteByGuid(Guid Id)
         {
             var account = await _context.Accounts.FindAsync(Id);
             if (account == null)
-                throw new NotFoundException("Account not found.");
+                return false;
+
             account.IsDeleted = true;
             account.DeletedAt = DateTime.UtcNow;
             await _context.SaveChangesAsync();
+            return true; 
         }
     }
     // Hard delete an account
@@ -116,13 +114,5 @@ namespace Calelytic.Core.Services
     // I believe that this should be added as a routine for the server where everyday at 00:00 the server checks the logs for all
     // deletion operations that happened 30 days (or any other period) before DateTime.UtcNow and implement a hard delete method
     // in that routine. When you read this comment, tell me how that could take place.
-
-
-
-
-    /* EDGE CASES:
-        1* Account for the case where the passed ID doesn't belong to any account.
-        2* Account for the case where the method doesn't need the class-wide AccountInstance. 
-    */
 
 }
